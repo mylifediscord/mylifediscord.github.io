f425bd7b915cd9d5e5c464f5fd3a535e11277844
Ylian1512 <yliansarra002@gmail.com>
2024-06-15 17:57:41 +0200

    Pause (i will work later)

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var btnA1 = document.getElementById('logBTN');
-    if (btnA1) {
-      btnA1.addEventListener('click', function() {
-        
-      });
-    } else {
-      console.error('Élément non trouvé.');
-    }
+    document.body.innerHTML = document.body.innerHTML + "<img id=\"web_CURSOR\" src=\"/assets/cursor.png\">"
+
+    const web_CURSOR = document.getElementById("web_CURSOR")
+    document.addEventListener("mousemove", function(e) {
+        let X = e.clientX;
+        let Y = e.clientY;
+        web_CURSOR.setAttribute("style", `transform: translateY(${Y - 400}px) translateX(${X}px);`)
+        console.log("CUR MOVE AHAHA")
+    })
+    
 });
