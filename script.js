2c962370be223e5465138ee35b523bfd77d36619
MyLife <mylife.disc@gmail.com>
2024-06-15 13:40:18 +0200

    For have access to all assets

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var btnA1 = document.getElementById('logBTN');
+    if (btnA1) {
+      btnA1.addEventListener('click', function() {
+        
+      });
+    } else {
+      console.error('Élément non trouvé.');
+    }
+});
