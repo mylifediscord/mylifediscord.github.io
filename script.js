a7558fa05b4a9c379e429320cb005907d6e9cc93
Ylian1512 <yliansarra002@gmail.com>
2024-06-16 14:55:44 +0200

    EN DEV

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.body.innerHTML = document.body.innerHTML + "<img id=\"web_CURSOR\" src=\"/assets/cursor.png\">"
-
-    const web_CURSOR = document.getElementById("web_CURSOR")
-    document.addEventListener("mousemove", function(e) {
-        let X = e.clientX;
-        let Y = e.clientY;
-        web_CURSOR.setAttribute("style", `transform: translateY(${Y - 400}px) translateX(${X}px);`)
-        console.log("CUR MOVE AHAHA")
-    })
     
 });
